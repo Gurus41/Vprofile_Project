0ba0bfb33c544245204b3161988215cfde1dea60
Gurus41 <149944950+Gurus41@users.noreply.github.com>
2024-05-12 11:00:58 +0530

    Update mysql.sh

--- vagrant/Automated_provisioning_WinMacIntel/mysql.sh
+++ vagrant/Automated_provisioning_WinMacIntel/mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DATABASE_PASS='admin123'
+DATABASE_PASS='Sai@143'
 sudo yum update -y
 sudo yum install epel-release -y
 sudo yum install git zip unzip -y
@@ -10,7 +10,7 @@ sudo yum install mariadb-server -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 cd /tmp/
-git clone -b main https://github.com/hkhcoder/vprofile-project.git
+git clone -b main https://github.com/Gurus41/Vprofile_Project.git
 #restore the dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
